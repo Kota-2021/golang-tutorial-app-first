870e2465f62853f0591c93ba3b9f79816e3570ab
kota-2021 <kota.lalala.2021@gmail.com>
2021-01-10 23:14:12 +0900

    update

--- main.go
+++ main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
-	"io"
+	"html/template"
 	"net/http"
 )
 
+type person struct {
+	First  string
+	Last   string
+	Saying string
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
 func main() {
-	http.HandleFunc("/", index)
+	http.HandleFunc("/", indexShow)
 	http.ListenAndServe(":8080", nil)
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "tmp")
+func indexShow(w http.ResponseWriter, r *http.Request) {
+
+	//io.WriteString(w, "some text hello world")
+
+	p1 := person{
+		First:  "HTTPserver",
+		Last:   "Start",
+		Saying: "powered by Golang",
+	}
+
+	p2 := person{
+		First:  "Hello",
+		Last:   "World !",
+		Saying: "Have a nice day. (≧▽≦)ノ",
+	}
+
+	xp := []person{p1, p2}
+
+	tpl.ExecuteTemplate(w, "index.gohtml", xp)
 }
